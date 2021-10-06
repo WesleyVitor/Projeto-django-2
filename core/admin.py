67dfc2e6e2a7206ade2d93b80275a1eee59172d0
WesleyVitor <wesleydemorais@outlook.com.br>
2021-10-06 17:18:37 -0300

    Criando modelos

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Funcionario, Cargo, Servico
 # Register your models here.
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ('nome','cargo','ativo','modificado','bio')
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('cargo','ativo','modificado')
+
+
+@admin.register(Servico)
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ('servico','icone','ativo')
