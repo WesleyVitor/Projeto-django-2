303d81b749fd63a0b1f9287838de603570b42909
WesleyVitor <wesleydemorais@outlook.com.br>
2021-10-11 16:20:36 -0300

    Modificando arquivo settings.py

--- fusion/settings.py
+++ fusion/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8r50o+^=xkxtw^(nghrx&4e2h669+!jjk@8+^1azv@ph89fco-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
